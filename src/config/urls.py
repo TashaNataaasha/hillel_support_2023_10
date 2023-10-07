9c69788c6a5ea963669aafdcdd8c4c66da33edf0
TashaNataaasha <tasha.zirka@gmail.com>
2023-10-07 14:55:15 +0300

    Provide the code quality

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
-from .exchange_rates import exchange_rates
 
+from .exchange_rates import exchange_rates
 
 urlpatterns = [
     # path("admin/", admin.site.urls),
