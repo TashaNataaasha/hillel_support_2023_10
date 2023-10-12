463f0ff6caa734587f370648f7055099e221beb7
TashaNataaasha <tasha.zirka@gmail.com>
2023-10-12 18:11:10 +0300

    Upload lesson 19

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,9 +1,9 @@
-from django.contrib import admin
 from django.urls import path
-
-from .exchange_rates import exchange_rates
+from users import api
 
 urlpatterns = [
-    # path("admin/", admin.site.urls),
-    path("exchange-rates/", exchange_rates),
+    path('users/all', api, name='all_users'),
+    path('users/create',api, name='create_user'),
+    path('issues/all', api, name='all_issues'),
+    path('issues/create', api, name='create_issue'),
 ]
