a334be8c8c189dc2f1fbc0718fce403cc31ceeb8
Nataliia <134959950+TashaNataaasha@users.noreply.github.com>
2023-10-12 22:09:00 +0300

    Revert "Upload lesson 19"

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,13 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from .exchange_rates import exchange_rates
-from users import api
 
+from .exchange_rates import exchange_rates
 
 urlpatterns = [
+    # path("admin/", admin.site.urls),
     path("exchange-rates/", exchange_rates),
-    path("users/all", api.users_all, name="all_users"),
-    path("users/create", api.users_create, name="create_user"),
-    path("issues/all", api.issues_all, name="all_issues"),
-    path("issues/create", api.issues_create, name="create_issue"),
 ]
