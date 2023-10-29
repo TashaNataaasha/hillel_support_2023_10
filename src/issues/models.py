1f1774fba1d8e74876fd7fc55576f888e0089f05
TashaNataaasha <tasha.zirka@gmail.com>
2023-10-29 19:21:33 +0200

    Upload lesson_23

--- src/issues/models.py
+++ src/issues/models.py
@@ -1,50 +1,17 @@
-from django.conf import settings
-from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-from shared.django import TimestampMixin
+class CustomUser(AbstractUser):
+    ROLES = [
+        ('junior', 'Junior'),
+        ('senior', 'Senior'),
+        ('default', 'Default'),
+    ]
+    role = models.CharField(max_length=10, choices=ROLES, default='default')
+    
+from django.db import models
 
-User = get_user_model()
-
-
-class Issue(TimestampMixin):
-    title = models.CharField(max_length=30)
-    body = models.CharField(max_length=255)
-    status = models.CharField(max_length=10)
-
-    junior = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.DO_NOTHING,
-        related_name="junior_issues",
-    )
-    senior = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.DO_NOTHING,
-        related_name="senior_issues",
-    )
-
-    class Meta:
-        db_table = "issues"
-
-
-# issue: Issue = Issue.objects.first()
-# issue.junior: ==> users.User
-
-# john: User = User.objects.get(email='john@email.com')
-# john.issues.first().messages.all()
-
-
-class Message(TimestampMixin):
-    content = models.CharField(max_length=100)
-
-    author = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.DO_NOTHING,
-        related_name="messages",
-    )
-    issue = models.ForeignKey(
-        "issues.Issue", on_delete=models.DO_NOTHING, related_name="messages"
-    )
-
-    class Meta:
-        db_table = "issues_messages"
+class Issue(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    status = models.CharField(max_length=20)
---
+++ src/issues/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('create-issue/', views.create_issue, name='create_issue'),
+    path('issues/', views.get_issues, name='get_issues'),
+    path('issues/<int:issue_id>/', views.get_issue_by_id, name='get_issue_by_id'),
+    
+]
